64b4a39deea49fa1b783dfbd68ef372ef2d7a455
Allan Zapata <alamezg@gmail.com>
2025-04-12 10:30:25 -0500

    feature: make placeholder data live generated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { AppSidebar } from "@/components/dashboard/app-sidebar"
 import { ChartAreaInteractive } from "@/components/dashboard/chart/chart-area-interactive"
 import { DataTable } from "@/components/dashboard/table/data-table"
@@ -8,20 +10,38 @@ import {
   SidebarProvider,
 } from "@/components/ui/sidebar"
 
-import React from "react";
+import React, {useEffect} from "react";
 import {temperatureColumns, TemperatureDataPoint} from "@/experiments/temperature/columns";
 
-import data from '@/../public/data.json'
+function useDynamicPlaceholderData(addFn: () => TemperatureDataPoint, interval = 3000) {
+  const [data, setData] = React.useState<TemperatureDataPoint[]>([])
 
-const temperatures: TemperatureDataPoint[] = data.temperature.map(point => {
-  return {
-    timestamp: point.ts,
-    value: point.value,
-    device: "Arduino MKR WiFi 1010",
-  } as unknown as TemperatureDataPoint;
-})
+  useEffect(() => {
+    const executeAndUpdate = () => {
+      const newPoint = addFn()
+      setData(prevPoints => [...prevPoints, newPoint])
+    }
+
+    executeAndUpdate()
+    const id = setInterval(executeAndUpdate, interval)
+
+    return () => clearInterval(id)
+  }, [interval]);
+
+  return data
+}
 
 export default function Page() {
+
+  const temperatures = useDynamicPlaceholderData((): TemperatureDataPoint => {
+    const randomTemperature = Math.random() * (28 - 19) + 19;
+    return {
+      timestamp: new Date().getTime(),
+      value: Number(randomTemperature.toFixed(1)),
+      device: "Cheap Sensor"
+    }
+  })
+
   return (
     <SidebarProvider
       style={
@@ -39,7 +59,7 @@ export default function Page() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+                <ChartAreaInteractive data={temperatures}/>
               </div>
               <div className="px-4 lg:px-6">
                 <DataTable columns={temperatureColumns} data={temperatures} />
--- src/components/dashboard/chart/chart-area-interactive.tsx
+++ src/components/dashboard/chart/chart-area-interactive.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import * as React from "react"
-import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+import {Area, AreaChart, CartesianGrid, XAxis} from "recharts"
 
-import { useIsMobile } from "@/hooks/use-mobile"
+import {useIsMobile} from "@/hooks/use-mobile"
 import {
   Card,
   CardAction,
@@ -33,408 +33,7 @@ import {parseTimeRange} from "@/lib/parse";
 
 export const description = "Un gráfico de área interactivo"
 
-const chartData = [
-  {
-    "ts": 1744405818761,
-    "value": "34.8"
-  },
-  {
-    "ts": 1744405816598,
-    "value": "34.0"
-  },
-  {
-    "ts": 1744405814460,
-    "value": "31.3"
-  },
-  {
-    "ts": 1744405812315,
-    "value": "24.5"
-  },
-  {
-    "ts": 1744405810145,
-    "value": "26.7"
-  },
-  {
-    "ts": 1744405808002,
-    "value": "20.2"
-  },
-  {
-    "ts": 1744405805874,
-    "value": "28.6"
-  },
-  {
-    "ts": 1744405803717,
-    "value": "25.7"
-  },
-  {
-    "ts": 1744405801577,
-    "value": "34.9"
-  },
-  {
-    "ts": 1744405799427,
-    "value": "29.5"
-  },
-  {
-    "ts": 1744405797282,
-    "value": "24.3"
-  },
-  {
-    "ts": 1744405795088,
-    "value": "25.4"
-  },
-  {
-    "ts": 1744405792945,
-    "value": "26.7"
-  },
-  {
-    "ts": 1744405790805,
-    "value": "28.2"
-  },
-  {
-    "ts": 1744405788662,
-    "value": "33.8"
-  },
-  {
-    "ts": 1744405786521,
-    "value": "24.7"
-  },
-  {
-    "ts": 1744405784355,
-    "value": "24.2"
-  },
-  {
-    "ts": 1744405782191,
-    "value": "26.6"
-  },
-  {
-    "ts": 1744405780175,
-    "value": "22.8"
-  },
-  {
-    "ts": 1744405777907,
-    "value": "32.5"
-  },
-  {
-    "ts": 1744405775765,
-    "value": "30.3"
-  },
-  {
-    "ts": 1744405773618,
-    "value": "32.1"
-  },
-  {
-    "ts": 1744405771442,
-    "value": "29.5"
-  },
-  {
-    "ts": 1744405769322,
-    "value": "22.4"
-  },
-  {
-    "ts": 1744405767081,
-    "value": "20.2"
-  },
-  {
-    "ts": 1744405764914,
-    "value": "21.7"
-  },
-  {
-    "ts": 1744405762747,
-    "value": "22.2"
-  },
-  {
-    "ts": 1744405760605,
-    "value": "30.1"
-  },
-  {
-    "ts": 1744405758439,
-    "value": "30.9"
-  },
-  {
-    "ts": 1744405756298,
-    "value": "30.1"
-  },
-  {
-    "ts": 1744405754158,
-    "value": "28.1"
-  },
-  {
-    "ts": 1744405751918,
-    "value": "24.8"
-  },
-  {
-    "ts": 1744405749661,
-    "value": "27.5"
-  },
-  {
-    "ts": 1744405747461,
-    "value": "26.9"
-  },
-  {
-    "ts": 1744405745341,
-    "value": "31.0"
-  },
-  {
-    "ts": 1744405743300,
-    "value": "26.4"
-  },
-  {
-    "ts": 1744405741047,
-    "value": "20.4"
-  },
-  {
-    "ts": 1744405738906,
-    "value": "32.0"
-  },
-  {
-    "ts": 1744405736741,
-    "value": "24.8"
-  },
-  {
-    "ts": 1744405734574,
-    "value": "27.7"
-  },
-  {
-    "ts": 1744405732335,
-    "value": "23.0"
-  },
-  {
-    "ts": 1744405730171,
-    "value": "25.4"
-  },
-  {
-    "ts": 1744405728033,
-    "value": "22.8"
-  },
-  {
-    "ts": 1744405725892,
-    "value": "30.3"
-  },
-  {
-    "ts": 1744405723644,
-    "value": "27.0"
-  },
-  {
-    "ts": 1744405721475,
-    "value": "25.6"
-  },
-  {
-    "ts": 1744405719336,
-    "value": "20.7"
-  },
-  {
-    "ts": 1744405717172,
-    "value": "27.1"
-  },
-  {
-    "ts": 1744405714956,
-    "value": "27.8"
-  },
-  {
-    "ts": 1744405712765,
-    "value": "21.9"
-  },
-  {
-    "ts": 1744405710624,
-    "value": "24.6"
-  },
-  {
-    "ts": 1744405708485,
-    "value": "31.6"
-  },
-  {
-    "ts": 1744405706322,
-    "value": "26.7"
-  },
-  {
-    "ts": 1744405704207,
-    "value": "26.0"
-  },
-  {
-    "ts": 1744405702032,
-    "value": "28.3"
-  },
-  {
-    "ts": 1744405699893,
-    "value": "25.7"
-  },
-  {
-    "ts": 1744405697725,
-    "value": "26.7"
-  },
-  {
-    "ts": 1744405695559,
-    "value": "26.3"
-  },
-  {
-    "ts": 1744405693442,
-    "value": "33.2"
-  },
-  {
-    "ts": 1744405691276,
-    "value": "33.6"
-  },
-  {
-    "ts": 1744405689109,
-    "value": "28.2"
-  },
-  {
-    "ts": 1744405686869,
-    "value": "30.3"
-  },
-  {
-    "ts": 1744405684702,
-    "value": "21.5"
-  },
-  {
-    "ts": 1744405682561,
-    "value": "25.2"
-  },
-  {
-    "ts": 1744405680417,
-    "value": "22.9"
-  },
-  {
-    "ts": 1744405678265,
-    "value": "20.4"
-  },
-  {
-    "ts": 1744405676017,
-    "value": "25.7"
-  },
-  {
-    "ts": 1744405673881,
-    "value": "30.9"
-  },
-  {
-    "ts": 1744405671701,
-    "value": "23.6"
-  },
-  {
-    "ts": 1744405669558,
-    "value": "20.4"
-  },
-  {
-    "ts": 1744405667421,
-    "value": "23.0"
-  },
-  {
-    "ts": 1744405665258,
-    "value": "23.0"
-  },
-  {
-    "ts": 1744405663120,
-    "value": "33.1"
-  },
-  {
-    "ts": 1744405660981,
-    "value": "29.4"
-  },
-  {
-    "ts": 1744405658817,
-    "value": "26.6"
-  },
-  {
-    "ts": 1744405656680,
-    "value": "32.0"
-  },
-  {
-    "ts": 1744405654512,
-    "value": "30.5"
-  },
-  {
-    "ts": 1744405652361,
-    "value": "26.1"
-  },
-  {
-    "ts": 1744405650218,
-    "value": "20.2"
-  },
-  {
-    "ts": 1744405648055,
-    "value": "32.8"
-  },
-  {
-    "ts": 1744405645919,
-    "value": "30.4"
-  },
-  {
-    "ts": 1744405643783,
-    "value": "34.9"
-  },
-  {
-    "ts": 1744405641621,
-    "value": "22.6"
-  },
-  {
-    "ts": 1744405639458,
-    "value": "27.7"
-  },
-  {
-    "ts": 1744405637320,
-    "value": "32.6"
-  },
-  {
-    "ts": 1744405635158,
-    "value": "26.7"
-  },
-  {
-    "ts": 1744405633022,
-    "value": "20.5"
-  },
-  {
-    "ts": 1744405630859,
-    "value": "31.7"
-  },
-  {
-    "ts": 1744405628710,
-    "value": "23.3"
-  },
-  {
-    "ts": 1744405626570,
-    "value": "30.3"
-  },
-  {
-    "ts": 1744405624408,
-    "value": "20.3"
-  },
-  {
-    "ts": 1744405622269,
-    "value": "24.5"
-  },
-  {
-    "ts": 1744405620106,
-    "value": "32.2"
-  },
-  {
-    "ts": 1744405617968,
-    "value": "21.2"
-  },
-  {
-    "ts": 1744405615931,
-    "value": "29.5"
-  },
-  {
-    "ts": 1744405613542,
-    "value": "32.6"
-  },
-  {
-    "ts": 1744405611404,
-    "value": "27.3"
-  },
-  {
-    "ts": 1744405609240,
-    "value": "23.7"
-  },
-  {
-    "ts": 1744405607097,
-    "value": "30.5"
-  },
-  {
-    "ts": 1744405604846,
-    "value": "31.7"
-  }
-]
+import {TemperatureDataPoint} from "@/experiments/temperature/columns";
 
 const chartConfig = {
   visitors: {
@@ -450,23 +49,22 @@ const chartConfig = {
   },
 } satisfies ChartConfig
 
-export function ChartAreaInteractive() {
+
+interface ChartAreaInteractiveProps {
+  data: TemperatureDataPoint[]
+}
+
+export function ChartAreaInteractive({data}: ChartAreaInteractiveProps) {
   const isMobile = useIsMobile()
   const [timeRange, setTimeRange] = React.useState("automático")
 
+
   // TODO: new data is not filtered. This is designed to display days in the x axis
-  const filteredData = chartData.filter((item) => {
-    const date = new Date(item.ts)
-    const referenceDate = new Date("2024-06-30")
-    let daysToSubtract = 90
-    if (timeRange === "30d") {
-      daysToSubtract = 30
-    } else if (timeRange === "7d") {
-      daysToSubtract = 7
-    }
-    const startDate = new Date(referenceDate)
-    startDate.setDate(startDate.getDate() - daysToSubtract)
-    return date >= startDate
+  const filteredData = data.filter((item) => {
+    const date = new Date(item.timestamp)
+    const now = new Date();
+
+    return date.getSeconds() < now.getSeconds() + 10;
   })
 
   const parsedRange = parseTimeRange(timeRange);
@@ -492,6 +90,7 @@ export function ChartAreaInteractive() {
             <ToggleGroupItem value="30d">Últimos 30 días</ToggleGroupItem>
             <ToggleGroupItem value="7d">Últimos 7 días</ToggleGroupItem>
             <ToggleGroupItem value="1d">Últimas 24 horas</ToggleGroupItem>
+            <ToggleGroupItem value="auto">Automático</ToggleGroupItem>
           </ToggleGroup>
           <Select value={timeRange} onValueChange={setTimeRange}>
             <SelectTrigger
@@ -499,7 +98,7 @@ export function ChartAreaInteractive() {
               size="sm"
               aria-label="Select a value"
             >
-              <SelectValue placeholder="Last 3 months" />
+              <SelectValue placeholder="Last 3 months"/>
             </SelectTrigger>
             <SelectContent className="rounded-xl">
               <SelectItem value="30d" className="rounded-lg">
@@ -510,6 +109,9 @@ export function ChartAreaInteractive() {
               </SelectItem>
               <SelectItem value="1d" className="rounded-lg">
                 Últimas 24 horas
+              </SelectItem>
+              <SelectItem value="auto" className="rounded-lg">
+                Automático
               </SelectItem>
             </SelectContent>
           </Select>
@@ -547,7 +149,7 @@ export function ChartAreaInteractive() {
                 />
               </linearGradient>
             </defs>
-            <CartesianGrid vertical={false} />
+            <CartesianGrid vertical={false}/>
             <XAxis
               dataKey="ts"
               tickLine={false}
