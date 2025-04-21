13b4f1d194339f46832b4e8ec4b57968ce606ceb
azapg <allangameyt28@gmail.com>
2025-04-20 22:27:35 -0500

    feature: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <link rel="icon" type="image/x-icon" href="/icon.svg"/>
+      <title></title>
+    </head>
+
     <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
