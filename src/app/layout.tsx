27d6b6fc587e916410576190fd6c8c4cc54992e5
azapg <allangameyt28@gmail.com>
2025-05-10 13:55:11 -0500

    feature: add toasts for notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import React from "react";
 import {ThemeProvider} from "@/components/theme-provider";
+import {Toaster} from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
       disableTransitionOnChange
     >
       {children}
+      <Toaster />
     </ThemeProvider>
     </body>
     </html>
