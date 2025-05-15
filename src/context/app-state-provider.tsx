be11c446909b1f5d48c1b7738fb25647086371d1
azapg <allangameyt28@gmail.com>
2025-05-15 09:53:01 -0500

    feature: add app state provider
    This allows accessing important app data such as what experiment is the user currently viewing.

---
+++ src/context/app-state-provider.tsx
@@ -0,0 +1,33 @@
+import {createContext, FC, ReactNode, useContext, useState} from "react";
+
+interface AppStateContextType {
+  currentExperiment?: number;
+  setCurrentExperiment: (experiment: number) => void;
+}
+
+const AppStateContext = createContext<AppStateContextType | undefined>(undefined);
+
+export const useAppState = (): AppStateContextType => {
+  const context = useContext(AppStateContext);
+  if(!context) {
+    throw new Error('useAppState must be used within an AuthProvider');
+  }
+  return context;
+}
+
+interface AppStateProviderProps {
+  children: ReactNode;
+}
+
+export const AppStateProvider: FC<AppStateProviderProps> = ({ children }) => {
+  const [currentExperiment, setCurrentExperiment] = useState<number | undefined>(undefined)
+
+  return (
+    <AppStateContext.Provider value={{
+      currentExperiment,
+      setCurrentExperiment
+    }}>
+      {children}
+    </AppStateContext.Provider>
+  )
+}
